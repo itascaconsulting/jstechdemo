b927f84dcad61750b60070250619e0f3c31ff64c
Jason Furtney <jkfurtney@gmail.com>
2017-04-20 13:38:17 -0500

    better match at plastic radius but not correct

--- new_solver.js
+++ new_solver.js
@@ -46,6 +46,10 @@ show(u(1e6));
 show(u(6e6));
 show(u(7e6));
 
+var u_re = function (p_i,r) {
+ return {R:r0, u:r0**2*(1+nu)*(p0-p_i)/E/r}; // Kirsch solution
+}
+
 
 var u_r = function (p_i,r) {
     if (p_i > p_cr) {
@@ -61,8 +65,22 @@ var u_r = function (p_i,r) {
     var t0 = p0+c*cotphi-(p_i + c*cotphi*(R/r0)**Q);
     var t = (1-nu)/E*R**2*t0 + b*(1+nu)/E;
     var u_r = (1-nu)/E*(p_i*r**(Q+1)/r0**Q - p0*r) + t/r;
-    if (r>=R) {
-        return {R:R, u: r0**2*(1+nu)*(p0-p_i)/E/r};
+    //var u_r0 = (1-nu)/E*(p_i*r0**(Q+1)/r0**Q - p0*r0) + t/r0;
+    //var sigma_R = p0-b/R**2; // which is the critical stress..
+    //console.log("stress at R " + sigma_R);
+    //console.log("u at r0 " + u_r0);
+    //console.log("u at R " + u_R);
+
+    if (r>R) {
+        var u_R = (1-nu)/E*(p_i*R**(Q+1)/r0**Q - p0*R) + t/R; // disp at R
+        var u_re = R**2*(1+nu)*(p0-p_cr-p_i)/E/R;
+        //console.log("u at R " + u_R);
+        //console.log("u at Re " + u_re);
+
+        var du = u_R-u_re;
+        // stress match to Kirsch solution? not sure if this is correct
+        return {R:R, //u:R**2*(1+nu)*(p0-p_cr)/E/r};
+                u:R**2*(1+nu)*(p0-p_cr-p_i)/E/r+du};
     }
     return {R:R, u: u_r};
 }
@@ -87,21 +105,16 @@ var np_linspace = function (start,end,num,endpoint) {
 
 support_pressures = np_linspace(0,p0,25,true);
 reaction = support_pressures.map(u);
-console.log(reaction);
 reaction2 = support_pressures.map(function (d) { return u_r(d,r0).u; });
-console.log(reaction2);
 
 
-var depth = np_linspace(r0,r0+4.0,25,true);
-console.log(depth);
+var depth = np_linspace(r0,r0+4.0,50,true);
 u_depth = depth.map(function (d) { return u_r(0,d).u;});
-console.log(u_depth);
+u_depthe = depth.map(function (d) { return u_re(0,d).u;});
+
 
 var plot_xy = function (xarray, yarray) {
-    console.log(xarray);
-    console.log(yarray);
-
-    var margin = {top: 30, right: 20, bottom: 30, left: 50},
+    var margin = {top: 30, right: 20, bottom: 30, left: 70},
         width = 400 - margin.left - margin.right,
         height = 220 - margin.top - margin.bottom;
     var     x = d3.scale.linear().range([0, width]);
@@ -152,3 +165,7 @@ var plot_xy = function (xarray, yarray) {
 }
 
 plot_xy(depth, u_depth);
+//plot_xy(depth, u_depthe);
+
+plot_xy(reaction, support_pressures);
+//plot_xy(reaction2, support_pressures);
