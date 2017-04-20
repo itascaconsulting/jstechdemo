c173e9d31d5c31bc68185b9b6093194ea279de07
Jason Furtney <jkfurtney@gmail.com>
2017-04-20 11:50:25 -0500

    alg refactor

--- new_solver.js
+++ new_solver.js
@@ -55,12 +55,13 @@ var u_r = function (p_i,r) {
         return {R:r0, u:r0**2*(1+nu)*(p0-p_i)/E/r}; // Kirsch solution
     }
     var delta = Math.PI/4.0 + phi/2.0;
-    var delta2 = Math.PI/4.0 + phi/2.0;
     var Q = Math.tan(delta)/(Math.tan(delta-phi))-1;
-    var denom = (1+Math.tan(delta)**2)*(p_i + c*math.cot(phi));
-    var R = r0 * ((2*p0-sigma_cm+(1+Math.tan(delta)**2)*c*math.cot(phi))/denom)**(1/Q);
-    var b = ((Math.tan(delta)**2-1)*p0+sigma_cm)/(Math.tan(delta)**2+1)*R**2;
-    var t0 = p0+c*math.cot(phi)-(p_i + c*math.cot(phi)*(R/r0)**Q);
+    var tan2phi = Math.tan(Math.PI/4.0 + phi/2.0)**2
+    var cotphi = math.cot(phi);
+    var denom = (1+tan2phi)*(p_i + c*cotphi);
+    var R = r0 * ((2*p0-sigma_cm+(1+tan2phi)*c*cotphi)/denom)**(1/Q);
+    var b = ((tan2phi-1)*p0+sigma_cm)/(tan2phi+1)*R**2;
+    var t0 = p0+c*cotphi-(p_i + c*cotphi*(R/r0)**Q);
     var t = (1-nu)/E*R**2*t0 + b*(1+nu)/E;
     var u_r = (1-nu)/E*(p_i*r**(Q+1)/r0**Q - p0*r) + t/r;
     return {R:R, u: u_r};
@@ -77,4 +78,4 @@ show(u_r(7e6,r0));
 // show(u_r(0,r0+0.3));
 // show(u_r(0,7));
 
-//r.map(function (d) { return u_r(d,r0).u; })._data;
+console.log(r.map(function (d) { return u_r(d,r0).u; })._data);
